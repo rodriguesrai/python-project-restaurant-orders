3dc9b38e0e5e797a49839deeee88c92ed2cfb1ae
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-22 15:44:11 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,31 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+
+    ingredient = Ingredient("farinha")
+    assert ingredient.name == "farinha"
+
+    ingredient = Ingredient("tomate")
+    assert ingredient.name == "tomate"
+    assert ingredient.restrictions == set()
+
+    assert (
+        repr(Ingredient("queijo mussarela"))
+        == "Ingredient('queijo mussarela')"
+    )
+
+    ingredient1 = Ingredient("bacon")
+    ingredient2 = Ingredient("bacon")
+    assert ingredient1 == ingredient2
+
+    ingredient1 = Ingredient("caldo de carne")
+    ingredient2 = Ingredient("caldo de carne")
+    assert hash(ingredient1) == hash(ingredient2)
+
+    ingredient3 = Ingredient("cebola")
+    ingredient4 = Ingredient("cenoura")
+    assert hash(ingredient3) != hash(ingredient4)
